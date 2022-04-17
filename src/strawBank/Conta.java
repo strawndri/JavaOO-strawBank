843104cf0caf8d0a7eedc7725e46c38afaf28c90
Andrieli Luci <andrieliluci@gmail.com>
2022-04-17 07:36:16 -0300

    Métodos e retornos de valores

--- src/strawBank/Conta.java
+++ src/strawBank/Conta.java
@@ -5,4 +5,39 @@ class Conta {
 	int agencia;
 	int numero;
 	String titular;
+
+	public void deposita(double valor) {
+
+		saldo += valor;
+
+		// se quiser, também é possível escrever this.saldo para
+		// indicar que o salto é para a objeto de certa referência
+		// que está utilizando o método.
+	}
+	
+	public boolean verificaSaldo(double valor) {
+		if (saldo >= valor) {
+			return true;
+		}
+		return false;
+	}
+
+	public String saca(double valor) {
+		if (verificaSaldo(valor)) {
+			saldo -= valor;
+			return "Valor sacado: R$ " + valor + " | Saldo: R$" + saldo;
+		}
+		return "Não foi possível realidar o saque. Saldo atual é: R$" + saldo;
+	}
+
+	
+	public boolean transfere(double valor, Conta destino) {
+		if (verificaSaldo(valor)) {
+			saca(valor);
+			destino.deposita(valor);
+			return true;
+		}
+		return false; // não precisa criar um else, já que, se caso for true, o processedimento é
+		  			 // parado.
+	}
 }
--- src/strawBank/criaConta.java
+++ src/strawBank/criaConta.java
@@ -4,17 +4,23 @@ public class criaConta {
 	
 	public static void main(String[] args) {
 		
+		// referência ao objeto 01
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200.0;
 		
+		// referência ao objeto 02
 		Conta segundaConta = new Conta();
 		segundaConta.saldo = 1000.0;
 		
+		// referência ao objeto 01
 		Conta terceiraConta = primeiraConta;
 		
 		System.out.println("Primeira conta: " + primeiraConta.saldo);
 		System.out.println("Segunda conta: " + segundaConta.saldo);
 		System.out.println("Terceira conta: " + terceiraConta.saldo);
+		
+		// Diferentes referências podem estar associadas ao mesmo objeto,
+		// que é o caso da primeira e da terceira conta.
 		
 	}
 }
